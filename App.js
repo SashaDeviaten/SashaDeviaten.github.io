26af47d4c182068338504fc5ea4b2fc5dc5f6e9c
SashaDeviaten <sasha_deviaten@mail.ru>
2019-04-11 22:21:24 +0300

    try to fix router problem (in process)

--- App.js
+++ App.js
@@ -8,7 +8,14 @@ import { Provider } from 'react-redux';
 import Main from './core/Main';
 import {createStore} from "redux";
 import combinedReducer from "./core/reducers";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
+import Switch from "react-router-dom/es/Switch";
+import Page_Clients from "./Pages/Page_Clients/Page_Clients.js";
+import MainPage from "./Pages/MainPage/MainPage.js";
+import Page_Reviews from "./Pages/Page_Reviews/Page_Reviews.jsx";
+import Page_Discount from "./Pages/Page_Discount/Page_Discount.jsx";
+import Page_Admin from "./Pages/Page_Admin/Page_Admin.jsx";
+import Page_Master from "./Pages/Page_Master/Page_Master.js";
 
 
 const store = createStore(combinedReducer, {}, window.devToolsExtension ? window.devToolsExtension() : f => f);
@@ -16,7 +23,15 @@ const store = createStore(combinedReducer, {}, window.devToolsExtension ? window
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Main/>
+            <Switch>
+                <Route path="/" exact component={MainPage}/>
+                <Route path="/master" component={Page_Master}/>
+                <Route path="/clients" component={Page_Clients}/>
+                <Route path="/reviews" component={Page_Reviews}/>
+                <Route path="/discount" component={Page_Discount}/>
+                <Route path="/admin" component={Page_Admin}/>
+            </Switch>
+            {/*<Main/>*/}
         </BrowserRouter>
     </Provider>
 
